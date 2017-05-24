fc01759f3f664c1ed12ce1b2dbe2164ab9db55ef
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-05-24 15:34:33 +0700

    Sphere Online Judge 250 exercise

---
+++ Sphere Online Judge 250 Most Solved Marathon With No Overlapping Problem/README.md
@@ -0,0 +1,4 @@
+# For Sphere Online Judge 250 Exercise Marathon
+
+## RULE
+        - Never Overlap the problem, it means that you are not allowed to solved the next problems before finish the last one
--- pre-competition-phase-2/pamungkas/pupils.cpp
+++ pre-competition-phase-2/pamungkas/pupils.cpp
@@ -9,6 +9,50 @@
 using namespace std;
 
 int main(){
-
+    int s,n,nA1,nA2,nA3,nA4,nA5,nB1,nB2,nB3,nB4,nB5,n1,n2,n3,n4,n5 = 0;
+    int A[100];
+    int B[100];
+    cin >> n;
+    for (int i = 0; i <n ; ++i) {
+        cin>>s;
+        if(s==1){
+            nA1 +=1;
+            n1 +=1;
+        }else if(s==2){
+            nA2 +=1;
+            n2 += 1;
+        }else if(s==3){
+            nA3 +=1;
+            n3 += 1;
+        }else if(s==4){
+            nA4 +=1;
+            n4 += 1;
+        }else if(s==5){
+            nA5 +=1;
+            n5+=1;
+        }
+    }
+    for (int i = 0; i <n ; ++i) {
+        cin>>s;
+        if(s==1){
+            nB1 +=1;
+            n1 +=1;
+        }else if(s==2){
+            nB2 +=1;
+            n2 += 1;
+        }else if(s==3){
+            nB3 +=1;
+            n3 += 1;
+        }else if(s==4){
+            nB4 +=1;
+            n4 += 1;
+        }else if(s==5){
+            nB5 +=1;
+            n5+=1;
+        }
+    }
+    if((n1%2=1)or(n2%2=1)or(n3%2=1)or(n4%2=1)or(n5%2=1)){
+        cout << -1 << endl;
+    }
     return 0;
 }
