9e27c7d59e88578247d8dbecbc4c8e78b825f7e6
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-05-29 19:49:23 +0700

    DP annoying

--- Sphere Online Judge 250 Most Solved Marathon With No Overlapping Problem/AnakRantauMenderita/Pamungkas/20.ACODE.cpp
+++ Sphere Online Judge 250 Most Solved Marathon With No Overlapping Problem/AnakRantauMenderita/Pamungkas/20.ACODE.cpp
@@ -4,18 +4,43 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdint>
 
 using namespace std;
 
 int main(){
-    string s;
+    string s,dmy;
     cin >> s;
-    while(s != '0'){
-        
-
-
-
-
+    while((s[0] != '0')or(s.length() != 1)){
+        vector <int64_t> dp(s.length()+1);
+        fill(dp.begin(),dp.end(),0);
+        dp[0] = 1;
+        if(s[0] !='0'){
+            dp[1] = 1;
+        }
+        for (int i = 1; i <s.length(); ++i) {
+            if(s[i]!='0'){
+                dp[i+1] = dp[i];
+            }
+            dmy = "";
+            dmy = dmy + s[i-1] +s[i];
+//            cout << dmy << endl;
+            if(dmy[0] != '0'){
+                if(dmy[0] == '2'){
+                    if(dmy[1]<='6'){
+                        dp[i+1]+=dp[i-1];
+                    }
+                }else if(dmy[0] == '1'){
+                    dp[i+1]+=dp[i-1];
+                }
+            }
+        }
+//        for (int j = 0; j <dp.size() ; ++j) {
+//            cout << dp[j];
+//        }
+//        cout << endl;
+        cout << dp[s.length()] << endl;
         cin >> s;
+        dp.clear();
     }
 }
